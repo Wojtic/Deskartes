cb5ffc36cda9b3747ed3687f75b3e443e66628f1
Wojtic <vojtacerny012@gmail.com>
2023-12-25 22:24:54 +0100

    Tictac three computer

--- app/src/js/games/tictac.js
+++ app/src/js/games/tictac.js
@@ -1,16 +1,12 @@
 import { vw, vh, tictac_textury } from "../main.js";
 
+let online = true;
 let onTurn = true;
 let isX = true;
 let grid;
-let X, Y;
 let opaque;
 
-let game = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
+let game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 export const tictac = {
   name: "Piškvorky",
@@ -26,9 +22,130 @@ export const tictac = {
   ],
 };
 
+async function opponent() {
+  online = false;
+  if (isGameOver(game)) {
+    console.log("Konec");
+    return;
+  }
+  if (online) {
+    console.log("vyčkat tahu od serveru, dodělat");
+  } else {
+    let bestMoveIndex = findBestMove(game);
+    game[bestMoveIndex] = -1;
+    const char = await getChar(!isX ? "X" : "Y");
+    char.x = (((bestMoveIndex % 3) - 1) * 744) / 3;
+    char.y = (((bestMoveIndex - (bestMoveIndex % 3)) / 3 - 1) * 548) / 3;
+    grid.addChild(char);
+    if (isGameOver(game)) {
+      console.log("Konec");
+      return;
+    }
+    turn();
+  }
+}
+// Function to check if a move is valid in the current board configuration
+function isValidMove(board, index) {
+  return board[index] === 0;
+}
+
+// Function to check if the game has ended with a win/loss/draw
+function isGameOver(board) {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  for (const condition of winConditions) {
+    const [a, b, c] = condition;
+    if (board[a] !== 0 && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+
+  return !board.includes(0); // If no empty cell, it's a draw
+}
+
+// Function to evaluate the board state for the minimax algorithm
+function evaluate(board) {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  for (const condition of winConditions) {
+    const [a, b, c] = condition;
+    if (board[a] !== 0 && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return 0; // Game is still ongoing
+}
+
+function minimax(board, depth, maximizingPlayer) {
+  if (isGameOver(board)) {
+    return evaluate(board);
+  }
+
+  if (maximizingPlayer) {
+    let best = Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (isValidMove(board, i)) {
+        board[i] = -1;
+        best = Math.min(best, minimax(board, depth + 1, false));
+        board[i] = 0;
+      }
+    }
+    return best;
+  } else {
+    let best = -Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (isValidMove(board, i)) {
+        board[i] = 1;
+        best = Math.max(best, minimax(board, depth + 1, true));
+        board[i] = 0; // Undo move
+      }
+    }
+    return best;
+  }
+}
+
+function findBestMove(board) {
+  let bestVal = Infinity;
+  let bestMove = -1;
+
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === 0) {
+      board[i] = -1;
+      let moveVal = minimax(board, 1, false);
+      board[i] = 0;
+
+      if (moveVal < bestVal) {
+        bestVal = moveVal;
+        bestMove = i;
+      }
+    }
+  }
+
+  return bestMove;
+}
+
 function turn() {
   const getPos = (local) => {
-    const temp = local / (grid.width / 2);
+    const temp = local / (744 / 2);
     if (temp < -1 / 3) return -1;
     if (temp > 1 / 3) return 1;
     return 0;
@@ -37,9 +154,8 @@ function turn() {
   function beginTurn(e) {
     let pos = e.data.getLocalPosition(grid); // Nejake pocitani s maticemi, mozna se tomu vyhnout
     let coords = { x: getPos(pos["x"]), y: getPos(pos["y"]) };
-    if (game[coords.y + 1][coords.x + 1] == 0) {
+    if (game[(coords.y + 1) * 3 + coords.x + 1] == 0) {
       opaque.alpha = 0.5;
-      console.log(grid.width);
       opaque.x = coords.x * (744 / 3); // Musí se používat velikost před scale.set, je to na nic
       opaque.y = coords.y * (548 / 3);
       /*opaque.x = (coords.x * grid.width) / 3;
@@ -50,18 +166,24 @@ function turn() {
   function endTurn(e) {
     grid.off("mousemove", beginTurn);
     onTurn = false;
+    if (online) {
+      console.log("odeslat na server, dodelat");
+    }
+    opponent();
   }
 
   async function click(e) {
     let pos = e.data.getLocalPosition(grid); // Nejake pocitani s maticemi, mozna se tomu vyhnout
     let coords = { x: getPos(pos["x"]), y: getPos(pos["y"]) };
-    if (game[coords.y + 1][coords.x + 1] == 0) {
+    if (game[(coords.y + 1) * 3 + coords.x + 1] == 0) {
       const char = await getChar(isX ? "X" : "Y");
-      char.x = (coords.x * grid.width) / 3;
-      char.y = (coords.y * grid.height) / 3;
+      char.x = (coords.x * 744) / 3;
+      char.y = (coords.y * 548) / 3;
       grid.addChild(char);
 
-      game[coords.y + 1][coords.x + 1] = isX ? 1 : -1;
+      game[(coords.y + 1) * 3 + coords.x + 1] = isX ? 1 : -1;
+      console.log(game);
+      console.log(evaluate(game));
 
       endTurn();
       grid.off("pointerdown", click);
@@ -83,7 +205,8 @@ async function getChar(letter) {
   return char;
 }
 
-export async function createTicTacThree(online) {
+export async function createTicTacThree(isOnline) {
+  online = isOnline;
   const Game = new PIXI.Container();
   const white = new PIXI.Graphics();
   white.beginFill(0xffffff);
@@ -91,7 +214,6 @@ export async function createTicTacThree(online) {
   white.alpha = 1;
 
   grid = new PIXI.Sprite(tictac_textury["three_grid"]);
-  console.log(grid.width, grid.height);
   grid.scale.set((80 * vh) / grid.height);
   grid.anchor.set(0.5);
   grid.x = 50 * vw;
@@ -102,6 +224,7 @@ export async function createTicTacThree(online) {
   opaque.alpha = 0; // ještě nastavit scale
 
   if (onTurn) turn();
+  else opponent();
 
   Game.addChild(white);
   grid.addChild(opaque);
